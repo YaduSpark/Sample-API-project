9b4e828007079a38dcb1ac2091c68b51664e56e7
YaduSpark <yadu@sparksupport.com>
2022-07-07 18:28:35 +0530

    Pagination added

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from .models import category, manufacturer, product
 
 # Register your models here.
+
+
 @admin.register(category, manufacturer, product)
 class ProductAdmin(admin.ModelAdmin):
     pass
